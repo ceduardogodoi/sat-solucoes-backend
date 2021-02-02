29a46125064b1fea13e94cf077c4ee670653682d
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-02-02 00:30:04 -0300

    fix: formata código

--- src/main/java/br/com/satsolucoes/resource/PessoaResource.java
+++ src/main/java/br/com/satsolucoes/resource/PessoaResource.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.satsolucoes.model.Pessoa;
 import br.com.satsolucoes.repository.PessoaRepository;
 import br.com.satsolucoes.service.PessoaService;
-
+  
 @RestController
 @RequestMapping("/pessoa")
 @CrossOrigin
